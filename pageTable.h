f46b41a4c90c5e6172781fc55d8ee0c668dce7dd
Huy Tran <48108978+huytran2511@users.noreply.github.com>
2020-05-15 17:14:18 -0700

    Update pageTable.h

--- pageTable.h
+++ pageTable.h
@@ -1,17 +1,3 @@
-/**********************
-***********************
-Student 1: Huy Tran
-Red ID: 818608122
-
-Student 2: Hosun Yoo
-Red ID: 819543212
-
-CS-570
-a04
-pageTable.h
-***********************
-***********************/
-
 #ifndef _PAGETABLE_H_
 #define _PAGETABLE_H_
 
